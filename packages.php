18dafae90771769a976579b59465c0370facfa5d
adlerre <rene.adler@electric-force.net>
2012-10-25 10:02:40 +0000

    add packages webintf fix packes url

---
+++ packages.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * $Id$
+ *
+ * @package    IBWUpdater
+ * @subpackage WebInterface
+ * @author     René Adler <rene.adler@tu-ilmenau.de>
+ * @version    $Revision$
+ */
+
+@include_once "./config/defaults.php";
+
+try {
+	$pkgMgr = PackageManager::getInstance();
+	echo $pkgMgr->buildXML(true, $pkgMgr->getPackagesForUserName($_GET["uid"]));
+} catch (Exception $e) {
+	echo $e->getMessage()."\n";
+	echo $e->getTraceAsString();
+}
+?>
