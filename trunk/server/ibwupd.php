be2de3fca9172569ecac3aa1f3082b158727e35e
adlerre <adlerre@8739d80e-6a82-4124-b353-9488188f9498>
2012-10-30 08:14:32 +0000

    introduce the CLI

---
+++ trunk/server/ibwupd.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+/**
+ * The IBWUpdater CLI
+ *
+ * @package    IBWUpdater
+ * @subpackage Main
+ * @author     René Adler <rene.adler@tu-ilmenau.de>
+ * @version    $Revision$
+ */
+@include_once "./config/defaults.php";
+
+new UpdaterCLI();
+?>
