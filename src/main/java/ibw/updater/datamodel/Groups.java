3ea3f0ad30384ea76c1b20aef250e48f56786818
adlerre <rene.adler@electric-force.net>
2016-08-29 07:59:02 +0200

    add containers

---
+++ src/main/java/ibw/updater/datamodel/Groups.java
@@ -0,0 +1,62 @@
+/*
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 3
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+package ibw.updater.datamodel;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Ren\u00E9 Adler (eagle)
+ *
+ */
+@XmlRootElement(name = "groups")
+public class Groups {
+
+	private List<Group> groups;
+
+	/**
+	 * 
+	 */
+	public Groups() {
+	}
+
+	/**
+	 * @param groups
+	 */
+	public Groups(List<Group> groups) {
+		this.groups = groups;
+	}
+
+	/**
+	 * @return the groups
+	 */
+	@XmlElement(name = "group")
+	public List<Group> getGroups() {
+		return groups;
+	}
+
+	/**
+	 * @param groups
+	 *            the groups to set
+	 */
+	public void setGroups(List<Group> groups) {
+		this.groups = groups;
+	}
+
+}
---
+++ src/main/java/ibw/updater/datamodel/Users.java
@@ -0,0 +1,60 @@
+/*
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 3
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+package ibw.updater.datamodel;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Ren\u00E9 Adler (eagle)
+ *
+ */
+@XmlRootElement(name = "users")
+public class Users {
+	
+	private List<User> users;
+
+	public Users() {
+
+	}
+
+	/**
+	 * @param users
+	 */
+	public Users(List<User> users) {
+		this.users = users;
+	}
+
+	/**
+	 * @return the users
+	 */
+	@XmlElement(name = "user")
+	public List<User> getUsers() {
+		return users;
+	}
+
+	/**
+	 * @param users
+	 *            the users to set
+	 */
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+}
