fae953e4c5750d69f29708b7f2c68e17f0650528
adlerre <rene.adler@electric-force.net>
2016-08-29 10:07:07 +0200

    implement edit and delete action

--- src/main/java/ibw/updater/frontend/resource/GroupResource.java
+++ src/main/java/ibw/updater/frontend/resource/GroupResource.java
@@ -21,9 +21,11 @@ import java.io.PrintStream;
 
 import javax.inject.Singleton;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -47,17 +49,61 @@ public class GroupResource {
 
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	public Response listGroups() {
+	public Response list() {
 		return Response.ok().status(Response.Status.OK).entity(GroupManager.get()).build();
 	}
 
 	@POST
 	@Path("add")
 	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	public Response addGroup(final Group group) {
+	public Response add(final Group group) {
 		try {
-			LOGGER.info("Add new group: " + group);
+			LOGGER.info("Add " + group);
 			GroupManager.save(group);
+			return Response.ok().status(Response.Status.OK).build();
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage(), e);
+			final StreamingOutput so = (OutputStream os) -> e.printStackTrace(new PrintStream(os));
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(so).build();
+		}
+	}
+
+	@POST
+	@Path("edit")
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public Response edit(final Group group) {
+		try {
+			LOGGER.info("Edit " + group);
+			GroupManager.update(group);
+			return Response.ok().status(Response.Status.OK).build();
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage(), e);
+			final StreamingOutput so = (OutputStream os) -> e.printStackTrace(new PrintStream(os));
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(so).build();
+		}
+	}
+
+	@POST
+	@Path("delete")
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public Response delete(final Group group) {
+		try {
+			LOGGER.info("Remove " + group);
+			GroupManager.delete(group);
+			return Response.ok().status(Response.Status.OK).build();
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage(), e);
+			final StreamingOutput so = (OutputStream os) -> e.printStackTrace(new PrintStream(os));
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(so).build();
+		}
+	}
+
+	@DELETE
+	@Path("delete/{gid}")
+	public Response delete(@PathParam("gid") final int gid) {
+		try {
+			LOGGER.info("Remove group with id " + gid);
+			GroupManager.delete(gid);
 			return Response.ok().status(Response.Status.OK).build();
 		} catch (Exception e) {
 			LOGGER.error(e.getMessage(), e);
