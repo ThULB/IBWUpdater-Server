c1157f5bdae9f51adfd5311298933d48664977a6
adlerre <rene.adler@uni-jena.de>
2016-11-07 14:35:05 +0100

    fix codestyle

--- src/main/resources/META-INF/resources/assets/js/ie10-viewport-bug-workaround.js
+++ src/main/resources/META-INF/resources/assets/js/ie10-viewport-bug-workaround.js
@@ -10,9 +10,9 @@
 	"use strict";
 
 	if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-		var msViewportStyle = document.createElement("style")
-		msViewportStyle.appendChild(document.createTextNode("@-ms-viewport{width:auto!important}"))
-		document.querySelector("head").appendChild(msViewportStyle)
+		var msViewportStyle = document.createElement("style");
+		msViewportStyle.appendChild(document.createTextNode("@-ms-viewport{width:auto!important}"));
+		document.querySelector("head").appendChild(msViewportStyle);
 	}
 
-})();
+}());
