75bc0ac763ba5fec986ae8fdaa2d61630c0d093d
René Adler <rene.adler@uni-jena.de>
2017-04-19 14:41:07 +0200

    remove unused import

--- src/main/java/ibw/updater/datamodel/User.java
+++ src/main/java/ibw/updater/datamodel/User.java
@@ -21,7 +21,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.persistence.Cacheable;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
